e04b6ce8b3bc5f1d9f9c0fe1957bd3eded2e3727
Keuntae Kim <keuntaekim1994@gmail.com>
2023-10-27 00:44:12 -0400

    10/26/2023 - Updated 'README.md' and added 'p3_program.py' that we can work on.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# csci6212_p3
+# CSCI 6212_12 - Project 3
 Project 3 in CSCI 6212-12 (Fall 2023 @ The George Washington University)
+
+## <Team 1> Members
+- Karan Patal
+- Oscar(Jiaye) Fang
+- Keuntae Kim
+- Edward Yeboah
+
+## Problem
+- Problem #0
+  - **Teleportation in Astro haunted galaxies**
+    - You have a teleporter that can take you from galaxy i to galaxy j. Cost to teleport is given by c(i,j), which can be arbitrary. Some galaxies are “astro-haunted” – this is specified by a(i) which can be 0 or 1 (1 means that that galaxy is “astro-haunted”). Give a polynomial time algorithm that minimizes the cost of going from galaxy 1 to galaxy n, such that you pass through no more than k astro-haunted galaxies. (You can assume that galaxies 1 and n are not astro-haunted.)
---
+++ p3_program.py
@@ -0,0 +1,26 @@
+################################################
+#   Team: Team 1                               #
+#   Members: Karan Patal / Oscar(Jiaye) Fang   #
+#           / Keuntae Kim / Edward Yeboah      #
+#   Course: CSCI 6212 - 12                     #
+#   Professor: Amrinder Arora                  #
+#                 < Project >                  #
+################################################
+
+"""
+- Problem #0
+  - Teleportation in Astro haunted galaxies
+    - You have a teleporter that can take you from galaxy i to galaxy j.
+    Cost to teleport is given by c(i,j), which can be arbitrary. Some galaxies
+    are “astro-haunted” – this is specified by a(i) which can be 0 or 1 (1 means
+    that that galaxy is “astro-haunted”). Give a polynomial time algorithm that
+    minimizes the cost of going from galaxy 1 to galaxy n, such that you pass
+    through no more than k astro-haunted galaxies. (You can assume that galaxies
+    1 and n are not astro-haunted.)
+"""
+
+def teleportation():
+    None
+
+if __name__ == "__main__":
+    None
